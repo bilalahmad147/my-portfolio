b781aeaad6766834ace181e2958d5c5f25e682b0
Muhammad Shahbaz <72626149+ShahbazSialvi@users.noreply.github.com>
2021-03-19 18:42:20 +0500

    Added aboutus module(not completed)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Components/Header/Header'
 import './App.css';
+import Aboutus from './Components/aboutus/aboutus' 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Aboutus />
     </div>
   );
 }
