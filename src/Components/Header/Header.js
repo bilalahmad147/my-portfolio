0bb96bf9b968b53116abdfaef0e2ca79329e0d52
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2021-03-31 21:55:26 +0500

    changes in header(Not completed)

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,35 +1,56 @@
-import React from 'react'
+import React from "react";
+import style from "./Header.module.css";
 
 function Header() {
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark nav">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#home">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#about">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#services">Services</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#work">My Work</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#blog">Blog</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          <img
+            src="https://dcassetcdn.com/design_img/2445125/516906/516906_12819630_2445125_c1caf247_image.png"
+            width="30"
+            height="30"
+            alt="Sab Tech Icon"
+          />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                About Us
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Contact Us
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Blog
+              </a>
+            </li>
+          </ul>
         </div>
-    );
+      </nav>
+    </div>
+  );
 }
 
 export default Header;
