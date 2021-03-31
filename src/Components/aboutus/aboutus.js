86358800d115fc34f40b64152558166f6c052942
Muhammad Shahbaz <72626149+ShahbazSialvi@users.noreply.github.com>
2021-03-31 19:06:14 +0500

    updated aboutus

--- src/Components/aboutus/aboutus.js
+++ src/Components/aboutus/aboutus.js
@@ -1,13 +1,30 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
-
+import { Container, Row, Col } from 'react-bootstrap'
+import './aboutus.css'
 
 
 function aboutus() {
     return (
         <div>
-                    
-            
+            <Container>
+                <Row>
+                    <h2>About US</h2>
+                </Row>
+                <Row>
+                    <Col>
+                    <Row>
+                         
+                    </Row>
+                    <Row>
+                        <h3>Our Motto</h3>
+                        <p>SAB tech is looking forward to work as well as helping others with there available resources. We offer free of cost trainings as well as internships to those who deserve.</p>
+                    </Row>
+                    </Col>
+                    <Col>
+                        <img src="https://avatars.githubusercontent.com/u/61399708?s=460&u=e6ae39dd149cdc436de8cbc9f8cb722dde510291&v=4"></img>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
